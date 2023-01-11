ea0a5adae259737924665937163689e83828e7bc
Shubham Soni <shubhamsonicse@gmail.com>
2023-01-12 04:23:15 +0530

    Gradle updated

--- app/src/main/java/com/modern/androidmodernui/MainActivity.kt
+++ app/src/main/java/com/modern/androidmodernui/MainActivity.kt
@@ -3,19 +3,9 @@ package com.modern.androidmodernui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.modern.androidmodernui.ui.theme.AndroidModernUITheme
 
 class MainActivity : ComponentActivity() {
